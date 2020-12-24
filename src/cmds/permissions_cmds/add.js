582aa4710f975be6abd13fce8b43f182ddccc7b8
Michele Azzolari <michele@azzolari.it>
2020-12-24 17:30:29 +0100

    cmd: permissions: add: implement ssh_options

--- src/cmds/permissions_cmds/add.js
+++ src/cmds/permissions_cmds/add.js
@@ -27,15 +27,17 @@ exports.builder = yargs => {
       demand: true,
       type: 'string'
     })
+    .option('ssh-options', {
+      alias: 's',
+      describe: 'SSH options',
+      demand: false,
+      type: 'string'
+    })
     .demandOption(['host', 'user']);
 };
 exports.handler = async argv => {
   try {
-    const { account, group, user, host } = argv;
-    const payload = {
-      user,
-      host
-    };
+    const { account, group, user, host, sshOptions } = argv;
     let url;
     if (account) {
       url = '/accounts/' + account + '/permissions';
@@ -44,6 +46,18 @@ exports.handler = async argv => {
     } else {
       console.error('One from account or group must be set');
       process.exit(2);
+    }
+    const payload = {
+      user,
+      host
+    };
+    if (sshOptions) {
+      try {
+        payload.ssh_options = JSON.parse(sshOptions);
+      } catch (e) {
+        console.error('Invalid ssh_options, it must be valid JSON');
+        process.exit(2);
+      }
     }
     const ret = await post(url, payload);
     outputJson(ret);
